7281e124ddd952f28b8eb9e082a15765778c5e7c
gcerv93 <gcerv93@protonmail.com>
2022-01-18 09:59:30 -0600

    add build_tree method

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -2,13 +2,13 @@
 
 # class for tree nodes
 class Node
-  attr_reader :data
+  attr_accessor :data, :left, :right
 
   include Comparable
-  def initialize(data, left_child = nil, right_child = nil)
+  def initialize(data)
     @data = data
-    @left_child = left_child
-    @right_child = right_child
+    @left = nil
+    @right = nil
   end
 
   def <=>(other)
@@ -18,9 +18,27 @@ end
 
 # class for the binary tree
 class Tree
-  attr_writer :root
+  attr_accessor :root
 
   def initialize(arr)
     @root = build_tree(arr)
+  end
+
+  # returns the level 0 root node
+  def build_tree(arr)
+    return nil if arr.empty?
+
+    arr_copy = arr.uniq.sort
+    mid = arr_copy.length / 2
+    root = Node.new(arr_copy[mid])
+    root.left = build_tree(arr_copy[0...mid])
+    root.right = build_tree(arr_copy[mid + 1..arr_copy.length])
+    root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
